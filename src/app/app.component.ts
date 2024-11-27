462a25cfc51f1ede1ea580b99501ecb857ed5654
tocxeen <tocxeen@outlook.com>
2024-11-27 16:50:26 +0200

    create sign up page, added forms, routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FrameComponent } from './shared/frame/frame.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FrameComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'facebook-login-page';
+  isLoggedIn = false;
 }
---
+++ src/app/shared/frame/frame.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-frame',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './frame.component.html',
+  styleUrl: './frame.component.css'
+})
+export class FrameComponent {
+
+}
